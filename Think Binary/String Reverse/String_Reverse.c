8b85b2c08e3c207ebc71afef40ce8bbd6a8b095a
Gurkamal Singh <gurkamal2323@gmail.com>
2016-11-10 23:36:36 +0530

    added string reverse

---
+++ Think Binary/String Reverse/String_Reverse.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string>
+#define MAX 100000
+void reverseString(char *, int);
+
+void reverseString(char *str, int len)
+{
+	if(len==0)
+	{
+		return;
+	}
+	reverseString(str+1,len-2);
+	char temp;
+	temp=str[0];
+	str[0]=str[len-1];
+	str[len-1]=temp;
+}
+
+int main()
+{
+	char str[MAX];
+	printf("Enter the word\n");
+	scanf("%s",str);
+	int i,len;
+	len=str.length();
+	reverseString(str,len);
+	return 0;
+}
